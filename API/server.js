9a275f08bd405c775c344c53149053e250bfd91a
GregoryBartolo <gregory.bartolo@etu.unice.fr>
2021-09-08 12:56:59 +0200

    add cd card system

--- API/server.js
+++ API/server.js
@@ -16,15 +16,36 @@ function requestArduino() {
 		
 		data = body.split(",");
 		
-		var id = parseInt(data[1]);
-		var temperature = parseFloat(data[2]);
-		var humidite = parseFloat(data[3]);
-		var batterie = parseFloat(data[4]);
-		var status = data[5];
+		if (data.length == 7) {
+			var id = parseInt(data[1]);
+			var temperature = parseFloat(data[2]);
+			var humidite = parseFloat(data[3]);
+			var batterie = parseFloat(data[4]);
+			var status = data[5];
+		} else if (data.length == 8) {
+			var old_data = data[1];
+			var id = parseInt(data[2]);
+			var temperature = parseFloat(data[3]);
+			var humidite = parseFloat(data[4]);
+			var batterie = parseFloat(data[5]);
+			var status = data[6];
+		} else {
+			console.log("Vérifier les données reçues !")
+		}
 		var dateTime = new Date();
 		dateTime = String(dateformat(dateTime, 'dd/mm/yyyy HH:MM:ss'));
 		
-		/*console.log(id);
+		// format to get old data
+		/*old_data = old_data.split("FLUSH")[1];
+		old_data = old_data.split("\r\r\n");
+		old_data[0] = old_data[0].slice(2);
+		old_data.pop();
+		old_data = old_data.reverse();*/
+		
+		/*console.log(data);
+		console.log("------------");
+		console.log(old_data);		
+		console.log(id);
 		console.log(temperature);
 		console.log(humidite);
 		console.log(batterie);
@@ -32,6 +53,22 @@ function requestArduino() {
 		console.log(dateTime);*/
 
 		let db = new sqlite3.Database('../surveillance_climatique.db')
+		
+		/*db.get('SELECT date FROM mesures WHERE id_capteur = ? AND id = (SELECT MAX(id) FROM mesures)', [id], (err, result) => {
+			if (err) {
+				return console.error(err.message);
+			}
+			
+			var date = new Date(result.date + "Z");
+			console.log(date);
+			
+			old_data.forEach(function(item) {
+				item = item.split("-");
+				new_date = date.setSeconds(date.getSeconds() + 5)
+				console.log(item);
+				console.log(new Date(new_date));
+			});
+		});*/
 		
 		db.get('SELECT limit_mini_temperature, limit_maxi_temperature, limit_mini_humidity, limit_maxi_humidity FROM capteurs WHERE id_capteur = ?', [id], (err, result) => {
 			if (err) {
